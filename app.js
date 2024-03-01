19d8e8f8f1902f94a977dbf983761df5c7cbf1f6
obaidullah1209 <mobaidullah554@gmail.com>
2024-02-29 19:20:11 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// question1
+var age = "I am 15 year old"
+alert(age)
+// question3
+var birthYear = "My Birth Year is 1990 \n Data type of my declared variable is number "
+document.write(birthYear) 
+// question4
+var message = "john doe ordered 5Tshirts on xyz store "
+document.write(message)
+
+
